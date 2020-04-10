70d16152c88dac7a75e87e113b96b26f51aa00bc
normandesjuniorzup <normandes.junior@zup.com.br>
2020-04-10 13:27:46 -0300

    Update lambda

--- lambda/main.js
+++ lambda/main.js
@@ -2,11 +2,25 @@
 
 const { Client } = require('pg');
 const client = new Client();
-client.connect();
+client
+  .connect()
+  .then(() => console.log('connected'))
+  .catch((err) => console.error('connection error', err.stack))
 
 module.exports.handler = async event => {
-  console.log('####### yeahhh!')
-  await client.end();
+  console.log('####### yeahhh!!')
+  
+  const accountNumber = '1111';
+  const query = {
+    name: 'fetch-account-sumary',
+    text: 'SELECT sum(amount) as total FROM account_activity WHERE account_number = $1',
+    values: [accountNumber]
+  }
+  
+  await client
+    .query(query)
+    .then(result => console.log(result.rows))
+    .catch(e => console.error(e.stack))
 
   return {
     statusCode: 200,
