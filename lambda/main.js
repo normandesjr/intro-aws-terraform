445d7d0042f9180e07459fd01956ce44b06995c9
normandesjuniorzup <normandes.junior@zup.com.br>
2020-04-11 16:28:36 -0300

    Refactor

--- lambda/main.js
+++ lambda/main.js
@@ -18,34 +18,31 @@ module.exports.handler = async event => {
     text: 'SELECT sum(amount) as total FROM account_activity WHERE account_number = $1',
     values: [accountNumber]
   }
-  
-  const result = {
-    'statusCode': 200,
-    'headers': {'Content-Type': 'application/json'}
-  }
-  const slackBody = {
-    'blocks': [{
-      'type': 'section',
-      'text': {
-        'type': 'mrkdwn',
-        'text': ''
-      }
-    }]
-  }
+
+  let text;
   try {
     const query = await client.query(fetchBalanceQuery);
     const balanceAccount = query.rows[0];
     
     if (balanceAccount && balanceAccount.total) {
-      slackBody.blocks[0].text.text = ':moneybag: Total balance R$'+ balanceAccount.total + ' for account number ' + accountNumber
+      text = ':moneybag: Total balance R$'+ balanceAccount.total + ' for account number ' + accountNumber
     } else {
-      slackBody.blocks[0].text.text = ':see_no_evil: Ops, account not found'
+      text = ':see_no_evil: Ops, account not found'
     }
   } catch (error) {
     console.error(error.stack)
-    slackBody.blocks[0].text.text = ':scream: Sorry, something terrible happened'
+    text = ':scream: Sorry, something terrible happened'
   }
 
-  result.body = JSON.stringify(slackBody)
-  return result;
+  return {
+    'statusCode': 200,
+    'headers': {'Content-Type': 'application/json'},
+    'body': JSON.stringify({'blocks': [{
+      'type': 'section',
+      'text': {
+        'type': 'mrkdwn',
+        'text': text
+      }
+    }]})
+  }
 };
